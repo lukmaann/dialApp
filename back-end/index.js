38e17d0f688dbeb83181f4f107abdea53d20e778
LNUAKX <lukmaannadaf@gmail.com>
2023-05-21 11:52:14 +0530

    added backed to save contacts

--- back-end/index.js
+++ back-end/index.js
@@ -9,6 +9,7 @@ dotenv.config();
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json({extended:true}))
 app.use(Cors())
 
 
@@ -17,17 +18,18 @@ app.use(Cors())
 mongoose.connect(process.env.MONGODB).then(()=>console.log("connected to db"))
 
 app.post("/add",(req,res)=>{
-  const {name,number}=req.body
- 
-const newContact= new contact ({
-name,number
-})
-newContact.save().then(()=>res.send("done"))
-
+ const {name,number}=req.body;
+ res.status(200).json({data:"recived"})
+ const newcontact=new contact({name,number});
+ newcontact.save().then(()=>console.log("data saved"))
 })
 
-app.get("/getData",(req,res)=>{
-  res.json({name:"lukmaan",number:91019})
+
+
+app.get("/getData",async (req,res)=>{
+  const contacts= await contact.find({})
+ res.status(200).json(contacts)
+  
   
 })
 
